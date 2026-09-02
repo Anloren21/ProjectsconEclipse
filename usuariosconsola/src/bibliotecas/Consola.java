a5ec5c6ff6358c279d9f84e0ae9dd4138cee20ff
Anloren21 <anlorerohu1996@gmail.com>
2026-09-02 23:15:36 +0200

    Mejoras de la consola

--- usuariosconsola/src/bibliotecas/Consola.java
+++ usuariosconsola/src/bibliotecas/Consola.java
@@ -3,8 +3,21 @@ package bibliotecas;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
+
 public class Consola {
 	private static final Scanner sc = new Scanner(System.in);
+	
+	public static void pl(Object mensaje) {
+		System.out.println(mensaje);
+	}
+
+	public static void p(Object mensaje) {
+		System.out.print(mensaje);
+	}
+	
+	public static void pf(String formato, Object...objetos) {
+		System.out.printf(formato, objetos);
+	}
 	
 	public static String pedirString(String mensaje) {
 		System.out.print(mensaje + ": ");
